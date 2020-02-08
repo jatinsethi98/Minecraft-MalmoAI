efa291061ccc9f667e296217edb98a626c12e6dd
agorne <33044268+agorne@users.noreply.github.com>
2020-02-07 21:55:53 -0800

    Add files via upload
    milestone1

---
+++ project.py
@@ -0,0 +1,154 @@
+from __future__ import print_function
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+# Tutorial sample #2: Run simple mission using raw XML
+
+from builtins import range
+import MalmoPython
+import os
+import sys
+import time
+import uuid
+import json
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+# More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
+
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Hello world!</Summary>
+              </About>
+              
+              <ServerSection>
+		<ServerInitialConditions>
+			<Time>
+				<StartTime>12000</StartTime>
+				<AllowPassageOfTime>false</AllowPassageOfTime>
+			</Time>
+		</ServerInitialConditions>
+                <ServerHandlers>
+                  <FlatWorldGenerator generatorString="3;7,2*3,22;1;lava_lake"/>
+			<DrawingDecorator>
+				<DrawBlock x="2" y="3" z="2" type="diamond_block"/>
+			</DrawingDecorator>
+                  <ServerQuitFromTimeUp timeLimitMs="30000"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Survival">
+                <Name>MalmoTutorialBot</Name>
+		<AgentStart>
+			<Placement x="1" y="4" z="1" yaw="0"/>
+            <Inventory>
+                <InventoryItem slot="0" type="diamond_pickaxe"/>
+            </Inventory>
+        </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ObservationFromGrid>
+                      <Grid name="floor3x3">
+                        <min x="-1" y="-1" z="-1"/>
+                        <max x="2" y="-1" z="2"/>
+                      </Grid>
+                  </ObservationFromGrid>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                   <AgentQuitFromTouchingBlockType>
+                    <Block type="diamond_block"/>
+                  </AgentQuitFromTouchingBlockType>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+my_client_pool = MalmoPython.ClientPool()
+my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10001)) #10000 in use - try 10001
+experimentID = str(uuid.uuid4())
+
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission(my_mission, my_client_pool, my_mission_record, 0, experimentID)
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+agent_host.sendCommand("pitch 0.2")
+# Loop until mission ends:
+len = 4
+while world_state.is_mission_running:
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+    if world_state.number_of_observations_since_last_state > 0:
+        msg = world_state.observations[-1].text
+        observations = json.loads(msg)
+        grid = observations.get(u'floor3x3', 0)
+        print(grid)        
+    agent_host.sendCommand("move 1")     
+    agent_host.sendCommand("strafe -1") 
+    agent_host.sendCommand("strafe -1") 
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
