03a1399d652822b3c5118a9332bf98ab5e858692
Jatin Sethi <31502817+jatinsethi98@users.noreply.github.com>
2020-02-24 13:33:56 -0800

    Updated with a wider maze

--- project.py
+++ project.py
@@ -22,17 +22,21 @@ from __future__ import print_function
 
 from builtins import range
 import MalmoPython
+import logging
 import os
 import sys
 import time
 import uuid
 import json
-
+import tkinter as tk
+import malmoutils
+import random
 if sys.version_info[0] == 2:
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 else:
-    import functools
-    print = functools.partial(print, flush=True)
+    import functools    
+    import builtins
+    builtins.print = functools.partial(print, flush=True)
 
 # More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
 
@@ -44,24 +48,26 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               </About>
               
               <ServerSection>
-		<ServerInitialConditions>
-			<Time>
-				<StartTime>12000</StartTime>
-				<AllowPassageOfTime>false</AllowPassageOfTime>
-			</Time>
-		</ServerInitialConditions>
+        <ServerInitialConditions>
+            <Time>
+                <StartTime>12000</StartTime>
+                <AllowPassageOfTime>false</AllowPassageOfTime>
+            </Time>
+        </ServerInitialConditions>
                 <ServerHandlers>
                   <FlatWorldGenerator generatorString="3;7,2*3,22;1;lava_lake"/>
-			<DrawingDecorator>
-				<DrawBlock x="2" y="3" z="2" type="diamond_block"/>
-                <DrawCuboid x1="-8" y1="3" z1="-12" x2="8" y2="10" z2="-12" type="planks"/>
-                <DrawCuboid x1="-8" y1="3" z1="12" x2="8" y2="10" z2="12" type="planks"/>
-                <DrawCuboid x1="8" y1="3" z1="12" x2="8" y2="10" z2="-12" type="planks"/>
-                <DrawCuboid x1="-8" y1="3" z1="12" x2="-8" y2="10" z2="-12" type="planks"/>
+            <DrawingDecorator>
+                <DrawBlock x="2" y="3" z="2" type="diamond_block"/>
+                <DrawCuboid x1="-11" y1="3" z1="-12" x2="11" y2="10" z2="-12" type="planks"/>
+                
+                <DrawCuboid x1="-11 " y1="3" z1="15" x2="11" y2="10" z2="15" type="planks"/>
+                <DrawCuboid x1="11" y1="3" z1="14" x2="11" y2="10" z2="-14" type="planks"/>
+                <DrawCuboid x1="-11" y1="3" z1="14" x2="-11" y2="10" z2="-14" type="planks"/>
+
                 <DrawCuboid x1="5" y1="3" z1="9" x2="5" y2="10" z2="-11" type="stone"/>
                 <DrawCuboid x1="5" y1="3" z1="9" x2="-5" y2="10" z2="9" type="stone"/>
                 <DrawCuboid x1="-5" y1="3" z1="9" x2="-5" y2="10" z2="-11" type="stone"/>
-			</DrawingDecorator>
+            </DrawingDecorator>
                   <ServerQuitFromTimeUp timeLimitMs="10000"/>
                   <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
@@ -69,8 +75,8 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               
               <AgentSection mode="Survival">
                 <Name>MalmoTutorialBot</Name>
-		<AgentStart>
-			<Placement x="8" y="4" z="-11" yaw="0"/>
+        <AgentStart>
+            <Placement x="8" y="4" z="-11" yaw="0"/>
             <Inventory>
                 <InventoryItem slot="0" type="diamond_pickaxe"/>
             </Inventory>
@@ -91,9 +97,263 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               </AgentSection>
             </Mission>'''
 
+
+
+
+
+
+
+
+
+class TabQAgent(object):
+    """Tabular Q-learning agent for discrete state/action spaces."""
+
+    def __init__(self, actions=[], epsilon=0.1, alpha=0.1, gamma=1.0, debug=False, canvas=None, root=None):
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.training = True
+
+        self.logger = logging.getLogger(__name__)
+        if debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
+        self.logger.handlers = []
+        self.logger.addHandler(logging.StreamHandler(sys.stdout))
+
+        self.actions = actions
+        self.q_table = {}
+        self.canvas = canvas
+        self.root = root
+        
+        self.rep = 0
+
+    def loadModel(self, model_file):
+        """load q table from model_file"""
+        with open(model_file) as f:
+            self.q_table = json.load(f)
+
+    def training(self):
+        """switch to training mode"""
+        self.training = True
+
+
+    def evaluate(self):
+        """switch to evaluation mode (no training)"""
+        self.training = False
+
+    def act(self, world_state, agent_host, current_r ):
+        """take 1 action in response to the current world state"""
+        
+        obs_text = world_state.observations[-1].text
+        obs = json.loads(obs_text) # most recent observation
+        self.logger.debug(obs)
+        if not u'XPos' in obs or not u'ZPos' in obs:
+            self.logger.error("Incomplete observation received: %s" % obs_text)
+            return 0
+        current_s = "%d:%d" % (int(obs[u'XPos']), int(obs[u'ZPos']))
+        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
+        if current_s not in self.q_table:
+            self.q_table[current_s] = ([0] * len(self.actions))
+
+        # update Q values
+        if self.training and self.prev_s is not None and self.prev_a is not None:
+            old_q = self.q_table[self.prev_s][self.prev_a]
+            self.q_table[self.prev_s][self.prev_a] = old_q + self.alpha * (current_r
+                + self.gamma * max(self.q_table[current_s]) - old_q)
+
+        self.drawQ( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
+
+        # select the next action
+        rnd = random.random()
+        if rnd < self.epsilon:
+            a = random.randint(0, len(self.actions) - 1)
+            self.logger.info("Random action: %s" % self.actions[a])
+        else:
+            m = max(self.q_table[current_s])
+            self.logger.debug("Current values: %s" % ",".join(str(x) for x in self.q_table[current_s]))
+            l = list()
+            for x in range(0, len(self.actions)):
+                if self.q_table[current_s][x] == m:
+                    l.append(x)
+            y = random.randint(0, len(l)-1)
+            a = l[y]
+            self.logger.info("Taking q action: %s" % self.actions[a])
+
+        # send the selected action
+        agent_host.sendCommand(self.actions[a])
+        self.prev_s = current_s
+        self.prev_a = a
+
+        return current_r
+
+    def run(self, agent_host):
+        """run the agent on the world"""
+
+        total_reward = 0
+        current_r = 0
+        tol = 0.01
+        
+        self.prev_s = None
+        self.prev_a = None
+        
+        # wait for a valid observation
+        world_state = agent_host.peekWorldState()
+        while world_state.is_mission_running and all(e.text=='{}' for e in world_state.observations):
+            world_state = agent_host.peekWorldState()
+        # wait for a frame to arrive after that
+        num_frames_seen = world_state.number_of_video_frames_since_last_state
+        while world_state.is_mission_running and world_state.number_of_video_frames_since_last_state == num_frames_seen:
+            world_state = agent_host.peekWorldState()
+        world_state = agent_host.getWorldState()
+        for err in world_state.errors:
+            print(err)
+
+        if not world_state.is_mission_running:
+            return 0 # mission already ended
+            
+        assert len(world_state.video_frames) > 0, 'No video frames!?'
+        
+        obs = json.loads( world_state.observations[-1].text )
+        prev_x = obs[u'XPos']
+        prev_z = obs[u'ZPos']
+        print('Initial position:',prev_x,',',prev_z)
+        
+        # take first action
+        total_reward += self.act(world_state,agent_host,current_r)
+        
+        require_move = True
+        check_expected_position = True
+        
+        # main loop:
+        while world_state.is_mission_running:
+        
+            # wait for the position to have changed and a reward received
+            print('Waiting for data...', end=' ')
+            while True:
+                world_state = agent_host.peekWorldState()
+                if not world_state.is_mission_running:
+                    print('mission ended.')
+                    break
+                if len(world_state.rewards) > 0 and not all(e.text=='{}' for e in world_state.observations):
+                    obs = json.loads( world_state.observations[-1].text )
+                    curr_x = obs[u'XPos']
+                    curr_z = obs[u'ZPos']
+                    if require_move:
+                        if math.hypot( curr_x - prev_x, curr_z - prev_z ) > tol:
+                            print('received.')
+                            break
+                    else:
+                        print('received.')
+                        break
+            # wait for a frame to arrive after that
+            num_frames_seen = world_state.number_of_video_frames_since_last_state
+            while world_state.is_mission_running and world_state.number_of_video_frames_since_last_state == num_frames_seen:
+                world_state = agent_host.peekWorldState()
+                
+            num_frames_before_get = len(world_state.video_frames)
+            
+            world_state = agent_host.getWorldState()
+            for err in world_state.errors:
+                print(err)
+            current_r = sum(r.getValue() for r in world_state.rewards)
+          
+            if world_state.is_mission_running:
+                assert len(world_state.video_frames) > 0, 'No video frames!?'
+                num_frames_after_get = len(world_state.video_frames)
+                assert num_frames_after_get >= num_frames_before_get, 'Fewer frames after getWorldState!?'
+                frame = world_state.video_frames[-1]
+                obs = json.loads( world_state.observations[-1].text )
+                curr_x = obs[u'XPos']
+                curr_z = obs[u'ZPos']
+                print('New position from observation:',curr_x,',',curr_z,'after action:',self.actions[self.prev_a], end=' ') #NSWE
+                if check_expected_position:
+                    expected_x = prev_x + [0,0,-1,1][self.prev_a]
+                    expected_z = prev_z + [-1,1,0,0][self.prev_a]
+                    if math.hypot( curr_x - expected_x, curr_z - expected_z ) > tol:
+                        print(' - ERROR DETECTED! Expected:',expected_x,',',expected_z)
+                        input("Press Enter to continue...")
+                    else:
+                        print('as expected.')
+                    curr_x_from_render = frame.xPos
+                    curr_z_from_render = frame.zPos
+                    print('New position from render:',curr_x_from_render,',',curr_z_from_render,'after action:',self.actions[self.prev_a], end=' ') #NSWE
+                    if math.hypot( curr_x_from_render - expected_x, curr_z_from_render - expected_z ) > tol:
+                        print(' - ERROR DETECTED! Expected:',expected_x,',',expected_z)
+                        input("Press Enter to continue...")
+                    else:
+                        print('as expected.')
+                else:
+                    print()
+                prev_x = curr_x
+                prev_z = curr_z
+                # act
+                total_reward += self.act(world_state, agent_host, current_r)
+                
+        # process final reward
+        self.logger.debug("Final reward: %d" % current_r)
+        total_reward += current_r
+
+        # update Q values
+        if self.training and self.prev_s is not None and self.prev_a is not None:
+            old_q = self.q_table[self.prev_s][self.prev_a]
+            self.q_table[self.prev_s][self.prev_a] = old_q + self.alpha * ( current_r - old_q )
+            
+        self.drawQ()
+    
+        return total_reward
+        
+    def drawQ( self, curr_x=None, curr_y=None ):
+        if self.canvas is None or self.root is None:
+            return
+        self.canvas.delete("all")
+        action_inset = 0.1
+        action_radius = 0.1
+        curr_radius = 0.2
+        action_positions = [ ( 0.5, 1-action_inset ), ( 0.5, action_inset ), ( 1-action_inset, 0.5 ), ( action_inset, 0.5 ) ]
+        # (NSWE to match action order)
+        min_value = -20
+        max_value = 20
+        for x in range(world_x):
+            for y in range(world_y):
+                s = "%d:%d" % (x,y)
+                self.canvas.create_rectangle( (world_x-1-x)*scale, (world_y-1-y)*scale, (world_x-1-x+1)*scale, (world_y-1-y+1)*scale, outline="#fff", fill="#000")
+                for action in range(4):
+                    if not s in self.q_table:
+                        continue
+                    value = self.q_table[s][action]
+                    color = int( 255 * ( value - min_value ) / ( max_value - min_value )) # map value to 0-255
+                    color = max( min( color, 255 ), 0 ) # ensure within [0,255]
+                    color_string = '#%02x%02x%02x' % (255-color, color, 0)
+                    self.canvas.create_oval( (world_x - 1 - x + action_positions[action][0] - action_radius ) *scale,
+                                             (world_y - 1 - y + action_positions[action][1] - action_radius ) *scale,
+                                             (world_x - 1 - x + action_positions[action][0] + action_radius ) *scale,
+                                             (world_y - 1 - y + action_positions[action][1] + action_radius ) *scale, 
+                                             outline=color_string, fill=color_string )
+        if curr_x is not None and curr_y is not None:
+            self.canvas.create_oval( (world_x - 1 - curr_x + 0.5 - curr_radius ) * scale, 
+                                     (world_y - 1 - curr_y + 0.5 - curr_radius ) * scale, 
+                                     (world_x - 1 - curr_x + 0.5 + curr_radius ) * scale, 
+                                     (world_y - 1 - curr_y + 0.5 + curr_radius ) * scale, 
+                                     outline="#fff", fill="#fff" )
+        self.root.update()
+
+
+
 # Create default Malmo objects:
 
 agent_host = MalmoPython.AgentHost()
+
+agent_host.addOptionalFloatArgument('alpha',
+    'Learning rate of the Q-learning agent.', 0.1)
+agent_host.addOptionalFloatArgument('epsilon',
+    'Exploration rate of the Q-learning agent.', 0.01)
+agent_host.addOptionalFloatArgument('gamma', 'Discount factor.', 1.0)
+agent_host.addOptionalFlag('load_model', 'Load initial model from model_file.')
+agent_host.addOptionalStringArgument('model_file', 'Path to the initial model file', '')
+agent_host.addOptionalFlag('debug', 'Turn on debugging.')
+
 try:
     agent_host.parse( sys.argv )
 except RuntimeError as e:
@@ -104,38 +364,100 @@ if agent_host.receivedArgument("help"):
     print(agent_host.getUsage())
     exit(0)
 
-my_mission = MalmoPython.MissionSpec(missionXML, True)
-my_mission_record = MalmoPython.MissionRecordSpec()
-my_client_pool = MalmoPython.ClientPool()
-my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10001)) #10000 in use - try 10001
-experimentID = str(uuid.uuid4())
 
 
-# Attempt to start a mission:
-max_retries = 3
-for retry in range(max_retries):
-    try:
-        agent_host.startMission(my_mission, my_client_pool, my_mission_record, 0, experimentID)
-        break
-    except RuntimeError as e:
-        if retry == max_retries - 1:
-            print("Error starting mission:",e)
-            exit(1)
-        else:
-            time.sleep(2)
+# -- set up the python-side drawing -- #
+scale = 40
+world_x = 16
+world_y = 26
+root = tk.Tk()
+root.wm_title("Q-table")
+canvas = tk.Canvas(root, width=world_x*scale, height=world_y*scale, borderwidth=0, highlightthickness=0, bg="black")
+canvas.grid()
+root.update()
 
-# Loop until mission starts:
-print("Waiting for the mission to start ", end=' ')
-world_state = agent_host.getWorldState()
-while not world_state.has_mission_begun:
-    print(".", end="")
-    time.sleep(0.1)
-    world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print("Error:",error.text)
+if agent_host.receivedArgument("test"):
+    num_maps = 1
+else:
+    num_maps = 30000
 
-print()
-print("Mission running ", end=' ')
+for imap in range(num_maps):
+
+    # -- set up the agent -- #
+    actionSet = ["movenorth 1", "movesouth 1", "movewest 1", "moveeast 1"]
+
+    agent = TabQAgent(
+        actions=actionSet,
+        epsilon=agent_host.getFloatArgument('epsilon'),
+        alpha=agent_host.getFloatArgument('alpha'),
+        gamma=agent_host.getFloatArgument('gamma'),
+        debug = agent_host.receivedArgument("debug"),
+        canvas = canvas,
+        root = root)
+
+    my_mission = MalmoPython.MissionSpec(missionXML, True)
+    my_mission.removeAllCommandHandlers()
+    my_mission.allowAllDiscreteMovementCommands()
+    my_mission.requestVideo( 320, 240 )
+    my_mission.setViewpoint( 1 )
+
+    my_mission_record = MalmoPython.MissionRecordSpec()
+
+    my_clients = MalmoPython.ClientPool()
+    my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
+
+    # Attempt to start a mission:
+    max_retries = 3
+    agentID = 0
+    expID = 'tabular_q_learning'
+
+    num_repeats = 150
+    cumulative_rewards = []
+
+    for i in range(num_repeats):
+
+        print("\nMap %d - Mission %d of %d:" % ( imap, i+1, num_repeats ))
+
+        for retry in range(max_retries):
+            try:
+                agent_host.startMission( my_mission, my_clients, my_mission_record, agentID, "%s-%d" % (expID, i) )
+                break
+            except RuntimeError as e:
+                if retry == max_retries - 1:
+                    print("Error starting mission:",e)
+                    exit(1)
+                else:
+                    time.sleep(2)
+
+        # Loop until mission starts:
+        print("Waiting for the mission to start ", end=' ')
+        world_state = agent_host.getWorldState()
+        while not world_state.has_mission_begun:
+            print(".", end="")
+            time.sleep(0.1)
+            world_state = agent_host.getWorldState()
+            for error in world_state.errors:
+                print("Error:",error.text)
+
+        print()
+
+        print("Mission running ", end=' ')
+        # -- run the agent in the world -- #
+        cumulative_reward = agent.run(agent_host)
+        print('Cumulative reward: %d' % cumulative_reward)
+        cumulative_rewards += [ cumulative_reward ]
+
+        # -- clean up -- #
+        time.sleep(0.5) # (let the Mod reset)
+
+    print("Done.")
+
+    print()
+    print("Cumulative rewards for all %d runs:" % num_repeats)
+    print(cumulative_rewards)
+
+
+"""
 #agent_host.sendCommand("pitch 0.2")
 # Loop until mission ends:
 len = 4
@@ -148,15 +470,14 @@ while world_state.is_mission_running:
         observations = json.loads(msg)
         grid = observations.get(u'floor3x3', 0)
         print(grid)        
-        
+   # agent_host.sendCommand("move 1")     
    # agent_host.sendCommand("strafe -1") 
    # agent_host.sendCommand("strafe -1") 
-    agent_host.sendCommand("move 1")     
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
         print("Error:",error.text)
-
+"""
 print()
 print("Mission ended")
 # Mission has ended.
